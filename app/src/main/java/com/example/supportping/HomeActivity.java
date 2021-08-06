aa1aae8849d7a3bf33adfbb5e0234285ad614095
limsaehyun <sh00710000@gmail.com>
2021-08-06 19:27:59 +0900

    Feat :: 메인 페이지와 XML 연동

--- app/src/main/java/com/example/supportping/HomeActivity.java
+++ app/src/main/java/com/example/supportping/HomeActivity.java
@@ -1,14 +1,40 @@
 package com.example.supportping;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ImageButton ib_add;
+
+    ArrayList<MainData> arrayList;
+    MainAdapter mainAdapter;
+    LinearLayout linearLayout;
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList = new ArrayList<>();
+
+        mainAdapter = new MainAdapter(arrayList, getApplicationContext());
+        recyclerView.setAdapter(mainAdapter);
+
+
+
     }
 }
