099eb662b5da4bacd6e2b0a0c3a3aa9aa2e4f6f5
limsaehyun <sh00710000@gmail.com>
2021-08-06 19:06:45 +0900

    Feat :: 메인 페이지

--- app/src/main/java/com/example/supportping/HomeActivity.java
+++ app/src/main/java/com/example/supportping/HomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class FirstActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_home);
     }
 }
