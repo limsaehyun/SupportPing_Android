2fb795cae6c9ea6a7f32d79ddef9c69829ab099f
saehyun <sh00710000@gmail.com>
2021-08-08 23:46:21 +0900

    Feat :: 마이페이지

---
+++ app/src/main/java/com/example/supportping/MyPageActivity.java
@@ -0,0 +1,13 @@
+package com.example.supportping;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
