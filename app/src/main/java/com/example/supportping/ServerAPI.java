f027f44e46a5525cf283e421ea8beb0951f436c4
saehyun <sh00710000@gmail.com>
2021-08-11 12:36:41 +0900

    Feat :: 로그인&회원가입 기능 구현

--- app/src/main/java/com/example/supportping/ServerAPI.java
+++ app/src/main/java/com/example/supportping/ServerAPI.java
@@ -1,6 +1,14 @@
 package com.example.supportping;
 
-import retrofit2.http.GET;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.Call;
 
 public interface ServerAPI {
+
+    @POST("login") //로그인
+    Call<RequestLogin> Login(@Body RequestLogin userData);
+
+    @POST("login") //로그인
+    Call<RequestLogin> Register(@Body RequestLogin userData);
 }
