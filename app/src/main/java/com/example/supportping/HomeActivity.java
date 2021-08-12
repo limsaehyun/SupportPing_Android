b3c7d84e73725f6461501729af0549ca2e1d559b
saehyun <sh00710000@gmail.com>
2021-08-12 19:50:32 +0900

    Fix :: 시작 세팅

--- app/src/main/java/com/example/supportping/HomeActivity.java
+++ app/src/main/java/com/example/supportping/HomeActivity.java
@@ -9,20 +9,30 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.gson.JsonObject;
+
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeActivity extends AppCompatActivity {
+
+    ServerResponse serverResponse;
 
     RecyclerView recyclerView;
     ImageButton ib_add;
 
-    ArrayList<MainData> arrayList;
     MainAdapter mainAdapter;
+
+    ArrayList<MainData> arrayList;
     LinearLayout linearLayout;
     LinearLayoutManager linearLayoutManager;
 
@@ -37,6 +47,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        System.out.println("토큰" + MainActivity.token);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         ib_menu = (ImageButton) findViewById(R.id.ib_menu);
@@ -63,21 +74,59 @@ public class HomeActivity extends AppCompatActivity {
         ib_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, PostActivity.class));
+//                startActivity(new Intent(HomeActivity.this, PostActivity.class));
             }
         });
 
 
     }
 
+    private void StartSetPost(ServerResponse serverResponse) {
+        int totleElements = serverResponse.getBoardInfos().size();
+        for(int i = 0; i < totleElements; i++) {
+            JsonObject jsonObject = serverResponse.getBoardInfos().get(i);
+
+            String startId = jsonObject.get("id").toString();
+            String startTitle = jsonObject.get("title").toString();
+            String startContent = jsonObject.get("content").toString();
+            String startPlace = jsonObject.get("place").toString();
+            String startpp = jsonObject.get("pp").toString();
+            String startmp = jsonObject.get("mp").toString();
+
+            System.out.println("테스트2" + startContent);
+
+            MainData mainData = new MainData(startTitle, startContent, startPlace, startmp, "10");
+            arrayList.add(mainData);
+
+            mainAdapter.notifyDataSetChanged();
+        }
+    }
+
+
     @Override
     protected void onResume() {
         super.onResume();
 
-        MainData mainData = new MainData("안드 오류 고쳐줘", "1315 임세현", "512호 변기 위", "30명", "5억 원");
-        arrayList.add(mainData);
-
+        arrayList.clear();
         mainAdapter.notifyDataSetChanged();
+
+        ServerAPI serverAPI = ApiProvider.getInstance().create(ServerAPI.class);
+
+        Call<ServerResponse> call = serverAPI.inquiry();
+
+        call.enqueue(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+                serverResponse = response.body();
+                StartSetPost(serverResponse);
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponse> call, Throwable t) {
+                System.out.println("실패");
+            }
+        });
+
     }
 
 }
