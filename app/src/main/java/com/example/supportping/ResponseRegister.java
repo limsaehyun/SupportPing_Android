7eccbe1f90c7539b88d0cb234aca8ccf3adced1a
saehyun <sh00710000@gmail.com>
2021-08-12 12:37:32 +0900

    Feat :: Provider 역할

---
+++ app/src/main/java/com/example/supportping/ResponseRegister.java
@@ -0,0 +1,13 @@
+package com.example.supportping;
+
+public class ResponseRegister {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
