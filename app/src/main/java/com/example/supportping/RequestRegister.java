4e17619144f185a775c0449f6af17993ddf83068
saehyun <sh00710000@gmail.com>
2021-08-12 12:38:34 +0900

    Feat :: 회원가입 Request

---
+++ app/src/main/java/com/example/supportping/RequestRegister.java
@@ -0,0 +1,37 @@
+package com.example.supportping;
+
+public class RequestRegister {
+    String username;
+    String password;
+    String name;
+
+    public RequestRegister(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
