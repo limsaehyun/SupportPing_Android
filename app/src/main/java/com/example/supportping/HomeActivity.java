f432a3ad9431e2c3c34672bd349bc1e240ef6c67
saehyun <sh00710000@gmail.com>
2021-08-12 16:52:49 +0900

    Feat :: Drawer Button

--- app/src/main/java/com/example/supportping/HomeActivity.java
+++ app/src/main/java/com/example/supportping/HomeActivity.java
@@ -1,14 +1,18 @@
 package com.example.supportping;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -22,10 +26,29 @@ public class HomeActivity extends AppCompatActivity {
     LinearLayout linearLayout;
     LinearLayoutManager linearLayoutManager;
 
+    ImageButton ib_menu;
+
+    DrawerLayout drawerLayout;
+
+    TextView tv_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ib_menu = (ImageButton) findViewById(R.id.ib_menu);
+        ib_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(drawerLayout.isDrawerOpen(Gravity.LEFT)) drawerLayout.closeDrawer(Gravity.LEFT);
+                else {
+                    drawerLayout.openDrawer(Gravity.LEFT);
+                }
+            }
+        });
 
         recyclerView = (RecyclerView) findViewById(R.id.rv);
         linearLayoutManager = new LinearLayoutManager(this);
