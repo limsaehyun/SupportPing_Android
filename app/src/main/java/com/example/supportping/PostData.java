172d1828305030384b695895b61586c3bfa1f4e7
saehyun <sh00710000@gmail.com>
2021-08-13 00:27:54 +0900

    Feat :: 글 정보를 저장하는 데이터 클래스

---
+++ app/src/main/java/com/example/supportping/PostData.java
@@ -0,0 +1,10 @@
+package com.example.supportping;
+
+public class PostData {
+    public static String[] id = new String[10000];
+    public static String[] title = new String[10000];
+    public static String[] content = new String[10000];
+    public static String[] place = new String[10000];
+    public static String[] pp = new String[10000];
+    public static String[] mp = new String[10000];
+}
