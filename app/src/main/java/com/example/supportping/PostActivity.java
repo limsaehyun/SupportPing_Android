8ecdd47515c85bc77b2ad71aaa9671571edbb107
saehyun <sh00710000@gmail.com>
2021-08-12 19:49:57 +0900

    Test :: Post 테스트 코드

--- app/src/main/java/com/example/supportping/PostActivity.java
+++ app/src/main/java/com/example/supportping/PostActivity.java
@@ -1,94 +1,133 @@
-package com.example.supportping;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class PostActivity extends AppCompatActivity {
-
-    private Spinner sp_floor;
-    private Spinner sp_location;
-
-
-
-    private TextView tv_result1;
-    private TextView tv_result2;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_post);
-
-        sp_floor = (Spinner)findViewById(R.id.sp_floor);
-        sp_location = (Spinner)findViewById(R.id.sp_location);
-
-
-        ArrayAdapter<CharSequence> dayadapter = ArrayAdapter.createFromResource(
-                this, R.array.floor, android.R.layout.simple_spinner_item);
-        dayadapter.setDropDownViewResource(R.layout.activity_post);
-        sp_floor.setAdapter(dayadapter);
-
-        tv_result2 = (TextView)findViewById(R.id.tv_result2);
-
-        ArrayAdapter<CharSequence> locationadapter = ArrayAdapter.createFromResource(
-                this, R.array.room, android.R.layout.simple_spinner_item);
-        locationadapter.setDropDownViewResource(R.layout.activity_post);
-        sp_location.setAdapter(locationadapter);
-
-        sp_floor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-
-                List<String> s = Arrays.asList(getResources().getStringArray(R.array.room));
-
-                if (pos == 1) {
-                    s = s.subList(0, 1);
-                    ArrayAdapter<String> flooradapter = new  ArrayAdapter<String>(PostActivity.this, android.R.layout.simple_spinner_item,s);
-                    sp_floor.setAdapter(flooradapter);
-                } else if (pos ==  2) {
-                    s = s.subList(5, 6);
-                    ArrayAdapter<String> flooradapter = new  ArrayAdapter<String>(PostActivity.this, android.R.layout.simple_spinner_item,s);
-                    sp_floor.setAdapter(flooradapter);
-                } else if (pos ==  3) {
-                    s = s.subList(0,28);
-                    ArrayAdapter<String> flooradapter = new  ArrayAdapter<String>(PostActivity.this, android.R.layout.simple_spinner_item,s);
-                    sp_floor.setAdapter(flooradapter);
-                }
-                else if (pos ==  4) {
-                    s = s.subList(0,28);
-                    ArrayAdapter<String> flooradapter = new  ArrayAdapter<String>(PostActivity.this, android.R.layout.simple_spinner_item,s);
-                    sp_floor.setAdapter(flooradapter);
-                }
-                else if (pos ==  5) {
-                    s = s.subList(0,28);
-                    ArrayAdapter<String> flooradapter = new  ArrayAdapter<String>(PostActivity.this, android.R.layout.simple_spinner_item,s);
-                    sp_floor.setAdapter(flooradapter);
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        sp_location.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                tv_result2.setText(parent.getItemAtPosition(position).toString());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-}
+//package com.example.supportping;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ImageButton;
+//import android.widget.Spinner;
+//import android.widget.TextView;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//import retrofit2.Response;
+//
+//public class PostActivity extends AppCompatActivity {
+//
+//    private static View PostButton;
+//    private Spinner floor;
+//    private EditText location;
+//    private TextView NowLocation, people;
+//    private Spinner person;
+//    private Button button;
+//    private ImageButton back;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_post);
+//
+//        floor = (Spinner) findViewById(R.id.floor);
+//        location = (EditText) findViewById(R.id.location);
+//        NowLocation = (TextView) findViewById(R.id.NowLocation);
+//        person = (Spinner) findViewById(R.id.person);
+//        people = (TextView) findViewById(R.id.people);
+//        button = (Button) findViewById(R.id.button);
+//        PostButton = (TextView) findViewById(R.id.PostButton);
+//        back = (ImageButton)findViewById(R.id.back);
+//
+//        back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+//
+//        floor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                NowLocation.setText(parent.getItemAtPosition(position).toString());
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//
+//        person.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                people.setText(parent.getItemAtPosition(position).toString());
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (location != null) {
+//                    NowLocation.setText(NowLocation.getText() + " " + location.getText().toString());
+//                }
+//            }
+//        });
+//        PostButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ServerAPI ServerApi = ApiProvider.getInstance().create(ServerAPI.class);
+//                Log.d("결과", "성공4");
+//                ServerResponse post = new ServerResponse("title", "content", "NowLocation", "people");
+//                Log.d("결과", "성공2");
+//                Call<ServerResponse> call = ServerApi.createPost(post);
+//                Log.d("결과", "성공3");
+//                call.enqueue(new Callback<ServerResponse>() {
+//                    @Override
+//                    public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+//                        Log.d("결과", "성공");
+//                        finish();
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<ServerResponse> call, Throwable t) {
+//                        Log.d("결과", "실패");
+//                    }
+//                });
+//            }
+//        });
+//
+//
+//    }
+//
+//    private void editPost(String data, String title, String content, String nowLocation, String people) {
+//        PostButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ServerAPI ServerAPI = ApiProvider.getInstance().create(ServerAPI.class);
+//                ServerResponse id = new ServerResponse("title", "content", "NowLocation", "people");
+//                Call<ServerResponse> call = ServerAPI.editPost(MainActivity.username, id);
+//                call.enqueue(new Callback<ServerResponse>() {
+//                    @Override
+//                    public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
+//                        Log.d("결과", "성공");
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<ServerResponse> call, Throwable t) {
+//                        Log.d("실패", "실패");
+//                    }
+//                });
+//            }
+//        });
+//    }
+//}
