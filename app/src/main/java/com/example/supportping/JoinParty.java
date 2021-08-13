8dd8b09f5e378f23127cfa7448b5c5691cb62570
saehyun <sh00710000@gmail.com>
2021-08-13 11:51:40 +0900

    Feat :: 참여 중인 파티 조회

---
+++ app/src/main/java/com/example/supportping/JoinParty.java
@@ -0,0 +1,14 @@
+package com.example.supportping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinParty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_party);
+    }
+}
