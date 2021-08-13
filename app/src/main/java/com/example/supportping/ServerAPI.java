cb3882ca2579f5ddaf0ab2201b4426ecdda934e8
saehyun <sh00710000@gmail.com>
2021-08-13 14:29:05 +0900

    Test :: 수정 위치 변경

--- app/src/main/java/com/example/supportping/ServerAPI.java
+++ app/src/main/java/com/example/supportping/ServerAPI.java
@@ -9,6 +9,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.Call;
 import retrofit2.http.Path;
@@ -30,9 +31,14 @@ public interface ServerAPI {
     @POST("user/enter/{board-id}") // 파티 참여
     Call<ServerRequest> joinPost(@Header("Authorization") String token, @Path("board-id") int board_id);
 
+    @PATCH("user/update/{board-id}") // 게시글 수정
+    Call<ServerRequest> EditPost(@Header("Authorization") String token, @Body ServerRequest serverRequest, @Path("board-id") int board_id);
+
     @GET("user/me/{user-id}") // 게시글 조회
     Call<ServerResponse> seePost(@Header("Authorization") String token, @Path("user-id") int user_id);
 
     @DELETE("user/delete/{board-id}") // 게시글 삭제
     Call<ServerRequest> deletePost(@Header("Authorization") String token, @Path("board-id") int board_id);
+
+
 }
